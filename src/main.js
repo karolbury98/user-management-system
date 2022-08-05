cf8720710137ebd4409261fc48c30c9c8cb9eaf7
karolbury98 <karolbury98@gmail.com>
2022-08-05 13:49:17 +0200

    added routing

--- src/main.js
+++ src/main.js
@@ -1,7 +1,18 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+//import store from "./store";
+
+//bootstrap
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+
+//fontawesome
+import "../node_modules/@fortawesome/fontawesome-free/css/all.css";
+
+import "./styles.css";
 
 createApp(App)
-  .use(router)
-  .mount('#app')
+//.use(store)
+.use(router)
+.mount("#app");
--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,42 @@
-import { createRouter, createWebHistory } from 'vue-router'
-
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../views/Home.vue";
+import UserManager from "../views/UserManager.vue";
+import AddUser from "../views/AddUser.vue";
+import EditUser from "../views/EditUser.vue";
+import ViewUser from "../views/ViewUser.vue";
 
 const routes = [
-
-]
+  {
+    path: "/",
+    name: "Home",
+    redirect: "/users",
+    component: Home,
+  },
+  {
+    path: "/users",
+    name: "UserManager",
+    component: UserManager,
+  },
+  {
+    path: "/users/add",
+    name: "AddUser",
+    component: AddUser,
+  },
+  {
+    path: "/users/edit:userId",
+    name: "EditUser",
+    component: EditUser,
+  },
+  {
+    path: "/users/view:userId",
+    name: "ViewUser",
+    component: ViewUser,
+  },
+];
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes
-})
+  history: createWebHistory(),
+  routes,
+});
 
-export default router
+export default router;
